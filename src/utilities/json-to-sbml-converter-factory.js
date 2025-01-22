109dedffa66db25041b9c90115b1f6e8ece24416
Umut Utku ERŞAHİNCE <ersahinceumut@gmail.com>
2025-01-22 12:49:13 +0300

    fix syntax error

--- src/utilities/json-to-sbml-converter-factory.js
+++ src/utilities/json-to-sbml-converter-factory.js
@@ -123,7 +123,7 @@ module.exports = function () {
                 if(simulationData["constant"] !== null)
                     comp.setConstant(simulationData["constant"]);
                 if(simulationData["spatialDimensions"] !== null)
-                    comp.setSpacialDimension(simulationData["spatialDimensions"]);
+                    comp.setSpacialDimensions(simulationData["spatialDimensions"]);
             }
             if(nodes[i]._private.data.label)
                 comp.setName(nodes[i]._private.data.label)
